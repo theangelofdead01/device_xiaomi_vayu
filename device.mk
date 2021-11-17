987b1eb1f69e20c8cd1318154199fc6f597f104f
Nauval Rizky <enuma.alrizky@gmail.com>
2021-11-17 16:14:55 +0700

    vayu: Add Touchfeature HAL and build it's HIDL from source
    * Needed for touch control in parts
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Change-Id: I365157177e37e0e23338b58ef720102bed74d7b3

--- device.mk
+++ device.mk
@@ -472,6 +472,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
     
+# Touchfeature
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.touchfeature@1.0.vendor
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
