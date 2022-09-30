237552b6a48f7f74cf37cc52327e62e6c1463e29
theangelofdead01 <rockinthecity142@gmail.com>
2022-09-29 21:49:17 -0500

    vayu: update vendor link

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@ if [[ -d "hardware/xiaomi" ]]; then
 rm -rf hardware/xiaomi
 fi
 
-git clone --depth 1 https://github.com/sanjeevstunner/vendor_xiaomi_vayu -b 13 vendor/xiaomi/vayu
+git clone --depth 1 https://github.com/theangelofdead01/vendor_xiaomi_vayu.git -b 13 vendor/xiaomi/vayu
 git clone --depth 1 https://github.com/sanjeevstunner/hardware_xiaomi -b 13 hardware/xiaomi
 git clone --depth 1 https://github.com/ArrowOS-Devices/android_kernel_xiaomi_vayu -b arrow-13.0 kernel/xiaomi/vayu
 
